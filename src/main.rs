634ecf9d98f3175b1eb0044a631c17efe30a002f
cassidea <cassidea@mailbox.org>
2019-09-03 23:19:06 +0200

    added reading of input file and converting to token list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let input_file = "enable1.txt";
+    let tokens = read_tokens(input_file).unwrap();
+    println!("Number of tokens {}", tokens.len());
+}
+
+fn read_tokens(input_file: &str) -> Result<Box<Vec<String>>, io::Error> {
+    println!("Opening file");
+    let input = fs::read_to_string(input_file)?;
+    let result = input.lines().map(|x| String::from(x.trim())).collect();
+    Ok(Box::new(result))
 }
